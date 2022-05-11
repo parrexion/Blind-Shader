44e3d50e073d0a59ae8b9895a6b1820af2b47792
afrisk <schack-93@hotmail.com>
2022-05-11 15:28:17 +0200

    Finishing touches on the new version.

--- Assets/_Scripts/HoverButton.cs
+++ Assets/_Scripts/HoverButton.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]
 public class HoverButton : MonoBehaviour, IPointerEnterHandler {
 
 	public AudioClip hoverSfx;
 
 
 	void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData) {
-		AudioController.instance.PlaySfx(hoverSfx);
+		if(GetComponent<Button>().interactable)
+			AudioController.instance.PlaySfx(hoverSfx, 0.75f);
 	}
 }
